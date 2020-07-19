4ec21dd4b6e0c75c42112ca6b31bd01a53d5c75c
rushikesh420 <rushikeshnarhare17@gmail.com>
2020-07-19 12:18:00 -0500

    Updates

---
+++ precache-manifest.8a1ddd71a68dfa7160cf242e5e13249e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa9ccf8c1c285be4359e32ca01b6f7f",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "815094cdba42a39c198e",
+    "url": "/Calculator/static/css/main.e664b237.chunk.css"
+  },
+  {
+    "revision": "34d7322788fd6cf8a9c4",
+    "url": "/Calculator/static/js/2.96fe6b3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.96fe6b3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "815094cdba42a39c198e",
+    "url": "/Calculator/static/js/main.0f6a4ab9.chunk.js"
+  },
+  {
+    "revision": "5580ef9a49869de7f531",
+    "url": "/Calculator/static/js/runtime-main.47230f85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
